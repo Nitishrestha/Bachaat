be8f180c2e1f422e13c8735b983c8e8a2535f95c
nitish shrestha <nitishrestha8848@gmail.com>
2018-05-21 15:39:58 +0545

    Create User Register API

---
+++ src/main/java/com/kat/bachaat/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.kat.bachaat.controller;
+
+import com.kat.bachaat.model.User;
+import com.kat.bachaat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> add(@RequestBody User user) {
+        User user1 = userService.registerUser(user);
+        return new ResponseEntity(user1, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/kat/bachaat/service/UserService.java
@@ -0,0 +1,7 @@
+package com.kat.bachaat.service;
+
+import com.kat.bachaat.model.User;
+
+public interface UserService {
+    User registerUser(User user);
+}
